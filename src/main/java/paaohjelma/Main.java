97075f99d03a6b6003e5e3e8dd36be4934b08f0f
sallamarieini <sallamarieini@gmail.com>
2020-11-08 14:40:23 +0200

    Updated gradle.yml

--- src/main/java/paaohjelma/Main.java
+++ src/main/java/paaohjelma/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        
+        Varasto mehua = new Varasto(100.0);
+        Varasto olutta = new Varasto(100.0, 20.2);
 
     }
 }
